96a15466849f1bafbe1baa6aea69ceb4fe2999bf
jjb <3432536713@qq.com>
2020-04-04 23:23:44 +0800

    first commit

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author:jinjiaobo
+ * @date:2020/4/4
+ * @description:
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
